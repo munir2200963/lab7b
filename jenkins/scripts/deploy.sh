5ea83a4a4e6b8d434ea2d40e67d74f747bd58859
munir2200963 <116726239+munir2200963@users.noreply.github.com>
2024-07-22 19:40:18 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+# Check if a container with the name 'my-apache-php-app' is already running and remove it if necessary
+if [ $(docker ps -aq -f name=my-apache-php-app) ]; then
+    docker rm -f my-apache-php-app
+fi
+
 set -x
 docker run -d -p 9090:80 --name my-apache-php-app -v C:\\Users\\rudyh\\Desktop\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
